657447c2371da7654649bccf6f631e6a657b7aed
627km <rudals_jeon@naver.com>
2023-04-11 22:32:59 +0900

    docs: upload 230410 README.md

--- 230410/calcDate.js
+++ 230410/calcDate.js
@@ -3,6 +3,6 @@ const firstDay = new Date("2023-01-001");  // 시작한 날
 const today = new Date();  // 오늘
 
 let passedTime = today.getTime() - firstDay.getTime();  // 시작한 날부터 오늘까지 흐른 시간(밀리초)
-let passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));  // 밀리초를 일(date) 수로 계산
+let passedDay = Math.round(passedTime / (1000 * 60 * 60 * 24));  // 밀리초를 일(date) 수로 계산, round()는 반올림
 
 result.innerText = passedDay;
